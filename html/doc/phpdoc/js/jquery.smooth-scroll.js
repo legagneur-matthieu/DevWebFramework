d9a2c7d26190bb2487cc3ec020f55ae2e411a4e3
LEGAGNEUR Matthieu <legagneur.matthieu@gmail.com>
2017-09-12 03:37:21 +0200

    DWF : PHPDoc update

--- html/doc/phpdoc/js/jquery.smooth-scroll.js
+++ html/doc/phpdoc/js/jquery.smooth-scroll.js
@@ -1,25 +1,28 @@
-$(document).ready(function () {
+$(document).ready(function() {
     function filterPath(string) {
         return string
-                .replace(/^\//, '')
-                .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
-                .replace(/\/$/, '');
+            .replace(/^\//,'')
+            .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+            .replace(/\/$/,'');
     }
     var locationPath = filterPath(location.pathname);
-    $('a[href*=#]').each(function () {
+
+    $('a[href*=#]').each(function() {
         var thisPath = filterPath(this.pathname) || locationPath;
-        if (locationPath == thisPath
-                && (location.hostname == this.hostname || !this.hostname)
-                && this.hash.replace(/#/, '')) {
+        if (  locationPath == thisPath
+            && (location.hostname == this.hostname || !this.hostname)
+            && this.hash.replace(/#/,'') ) {
             var $target = $(this.hash), target = this.hash;
             if (target) {
-                $(this).click(function (event) {
+                $(this).click(function(event) {
                     if (!$(this.hash).offset()) {
                         return;
                     }
+
                     event.preventDefault();
                     position = $(this.hash).offset().top;
-                    $('html,body').animate({scrollTop: position}, 400, function () {
+
+                    $('html,body').animate({scrollTop: position}, 400, function() {
                         location.hash = target;
                     });
                 });

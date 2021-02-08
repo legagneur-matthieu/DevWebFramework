0620485c79ee52d8a71f41198b2ea3de5b2f1cd7
Rodolphe <Rodolphe@DESKTOP-9O3NP2L.home>
2021-02-08 10:40:34 +0100

    Correction orthographe #6 ( docform.php )

--- html/doc/class/docform.php
+++ html/doc/class/docform.php
@@ -6,32 +6,32 @@ form::input("Input de type text", "input_1");
 form::input("Input de type password", "input_2", "password");
 form::input("Input avec valeur initiale", "input_3", "text", "valeur initiale");
 form::datepicker("Un datepicker", "datepicker_1");
-form::select("Un selecteur", "select_1", array(
+form::select("Un sélecteur", "select_1", array(
     array(1, "Abricots"),
     array(2, "Poires", true), //Poires est selectioné par defaut
     array(3, "Pommes"),
 ));
 form::textarea("Un textarea", "ta_1");
-//création d"un CKEditor
+//création d'un CKEditor
 form::textarea("Un ckeditor", "ta_2");
 $cke = js::ckeditor("ta_2");
 
-//bouton de soumition
+//bouton de soumission
 form::submit("btn-primary");
 //fermeture du formulaire
 form::close_form();
 
-//execution du formulaire
+//exécution du formulaire
 if (isset($_POST["input_1"])) {
 
-    //recupere la date du datepicker au format US
+    //récupère la date du datepicker au format US
     $date = form::get_datepicker_us("datepicker_1");
-    //filtre les balises utilisé dans CKEditor, protection XSS
+    //filtre les balises utilisées dans CKEditor, protection XSS
     $ta_2 = $cke->parse($_POST["ta_2"]);
 
-    //message de succes ou erreur
+    //message de succès ou erreur
     js::alert("le formulaire a bien été soumis");
-    //redirection vers la page courante = rafraichisement de la page
+    //redirection vers la page courante = rafraichissement de la page
     js::redir("");
 }
 ?>

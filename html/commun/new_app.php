b131cf626eb32a1e739c3f3cb3fa850475ae1214
LEGAGNEUR Matthieu <legagneur.matthieu@gmail.com>
2021-05-14 15:52:10 +0200

    droits

--- html/commun/new_app.php
+++ html/commun/new_app.php
@@ -212,15 +212,15 @@ class new_app {
         $file_pages = $dir_class . "/pages.class.php";
         $file_config = $dir_class . "/config.class.php";
         $file_htaccess = $dir_class . "/.htaccess";
-        mkdir($dir);
+        mkdir($dir, 0775, true);
         $this->check_create_dir($dir);
-        mkdir($dir_class);
+        mkdir($dir_class, 0775, true);
         $this->check_create_dir($dir_class);
-        mkdir($dir_entity);
+        mkdir($dir_entity, 0775, true);
         $this->check_create_dir($dir_entity);
-        mkdir($dir_src);
+        mkdir($dir_src, 0775, true);
         $this->check_create_dir($dir_src);
-        mkdir($dir_src_compact);
+        mkdir($dir_src_compact, 0775, true);
         $this->check_create_dir($dir_src_compact);
         /* index_entity */
         $index_entity = '<?php header("Location: ../../index.php");';
@@ -257,7 +257,7 @@ class new_app {
         if (isset($_POST["srv"])) {
             $dir_services = $dir . "/services";
             $file_services = '<?php class index_service { /** * Cette classe est la première appelée, elle ouvre les variables de session et la connexion a la base de donnée, <br /> * redéfinie la time zone et fait appel à ces méthodes privées avant d\'appeler la class application (IDEM __construct()...) */ public function __construct() { try { $this->classloader(); include "../class/config.class.php"; if (isset($_REQUEST["service"])) { date_default_timezone_set("Europe/Paris"); $this->entityloader(); application::$_bdd = new bdd(); session::start(false); $this->serviceloader(); $this->security_purge(); $service = strtr($_REQUEST["service"], array("." => "", "/" => "", "\\\\" => "", "?" => "", "#" => "")); if (file_exists($service . ".service.php")) { $service = new $service(); } else { dwf_exception::throw_exception(622, array("_s_" => $service)); } } else { dwf_exception::throw_exception(621); } } catch (Exception $e) { dwf_exception::print_exception($e, "", true); } } /**     * Inclut toutes les classes du framework */ private function classloader() { spl_autoload_register(function($class) { $file = __DIR__ . "/../../../dwf/class/" . $class . ".class.php"; if (file_exists($file)) { require_once $file; } }); } /**     * Inclut les entités du projet */ private function entityloader() { spl_autoload_register(function($class) { $file = __DIR__ . "/../class/entity/" . $class . ".class.php"; if (file_exists($file)) { require_once $file; } }); } /**     * Inclut toutes les classes du dossier "service" se finissant par ".service.php" */ private function serviceloader() {spl_autoload_register(function($class) {$file = __DIR__ . "/" . $class . ".service.php";if (file_exists($file)) {require_once $file;}});}/**     * Supprime tous les fichiers se terminant par .php~ (trill) dans le dossier "service" <br /> * Cette fonction est recommandée sur les serveurs de production Linux pour des raisons de sécurité <br /> * certains hébergeurs tolèrent mal cette fonction, elle peut être désactivée en commentant la ligne "$this->security_purge();" dans le constructeur. */private function security_purge() {foreach (glob("*.php~") as $trill) {unlink($trill);}}}new index_service();';
-            mkdir($dir_services);
+            mkdir($dir_services, 0775, true);
             $this->check_create_dir($dir_services);
             $this->create_file($dir_services . "/index.php", $file_services);
         }
@@ -280,7 +280,6 @@ class new_app {
     private function check_create_dir($file_or_dir) {
         if (!file_exists($file_or_dir)) {
             js::alert($file_or_dir . " n'a pu être créé, merci de vérifier droits du dossier HTML");
-            exit();
         }
     }
 
@@ -300,7 +299,6 @@ class new_app {
             }
         } catch (Exception $exc) {
             echo $exc->getTraceAsString();
-            exit();
         }
     }
 
@@ -309,7 +307,7 @@ class new_app {
      * Vous pouvez créer vos propres classes avec cette extension pour les charger automatiquement avant de les utiliser dans votre application 
      */
     private function classloader() {
-        spl_autoload_register(function($class) {
+        spl_autoload_register(function ($class) {
             $file = __DIR__ . "/../../dwf/class/" . $class . ".class.php";
             if (file_exists($file)) {
                 require_once $file;

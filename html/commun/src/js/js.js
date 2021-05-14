a2e7015cf41507e35ef95ea923e84f9d76bc90bd
LEGAGNEUR Matthieu <legagneur.matthieu@gmail.com>
2021-05-14 15:19:30 +0200

    remove popover

--- html/commun/src/js/js.js
+++ html/commun/src/js/js.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
             $(".carousel-pause").attr("data-slide", "pause");
         }
     });
-    $('[data-toggle="popover"]').popover();
     $('[data-toggle="tooltip"]').tooltip();
     //parallax
     (function () {

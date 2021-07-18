b2933b591e8583ff94712c28c9fbafe72c9ff696
LEGAGNEUR Matthieu <legagneur.matthieu@gmail.com>
2021-07-18 18:27:03 +0200

    Reaveal 3.9.2 -> 4.2.1

--- dwf/class/reveal.class.php
+++ dwf/class/reveal.class.php
@@ -9,6 +9,12 @@
 class reveal {
 
     /**
+     * Permet de vérifier que la librairie reveal a bien été appelée qu'une fois.
+     * @var boolean Permet de vérifier que la librairie reveal a bien été appelée qu'une fois.
+     */
+    private static $_called = false;
+
+    /**
      * Cette classe permet de créer un diaporama avec la librairie reveal, Il n'est pas recommandé d'avoir plusieurs diaporamas sur la même page !
      * Les diaporamas s'organisent avec des balises "section"
      * @param int $width Largeur en px des slides
@@ -16,47 +22,42 @@ class reveal {
      * @param string $theme Théme des slides (white par defaut, beige,black,blood,league,noon,night,serif,simple,sky,solarized)
      */
     public function __construct($width = 600, $height = 600, $theme = "white") {
-        compact_css::get_instance()->add_css_file("../commun/src/js/reveal/css/reveal.css");
-        compact_css::get_instance()->add_css_file("../commun/src/js/reveal/css/theme/" . $theme . ".css");
-        compact_css::get_instance()->add_css_file("../commun/src/js/reveal/css/reveal_correctif.css");
-        echo html_structures::script("../commun/src/js/reveal/lib/js/classList.js") .
-        html_structures::script("../commun/src/js/reveal/lib/js/head.min.js") .
-        html_structures::script("../commun/src/js/reveal/js/reveal.js");
-        ?>
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $(".reveal .controls").ready(function () {
-                    $(".reveal,.slides").css("width", "<?= $width; ?>px").css("height", "<?= $height; ?>px");
-                    $(".reveal .controls").css("margin-top", "<?= (-800 + $height); ?>px").css("margin-left", "<?= ($width - 100); ?>px");
+        if (!self::$_called) {
+            $base = "../commun/src/js";
+            echo html_structures::link_in_body("$base/reveal/dist/reveal.css") .
+            html_structures::link_in_body("$base/reveal/dist/theme/$theme.css") .
+            html_structures::link_in_body("$base/reveal/plugin/highlight/monokai.css");
+            foreach ([
+        "$base/reveal/dist/reveal.js",
+        "$base/reveal/plugin/highlight/highlight.js",
+        "$base/reveal/plugin/markdown/markdown.js",
+        "$base/reveal/plugin/math/math.js",
+        "$base/reveal/plugin/notes/notes.js",
+        "$base/reveal/plugin/search/search.js",
+        "$base/reveal/plugin/zoom/zoom.js"
+            ] as $src) {
+                echo html_structures::script($src);
+            }
+            ?>
+            <script type="text/javascript">
+                $(document).ready(function () {
+                    $(".reveal")
+                            .css("width", '<?= $width ?>px')
+                            .css("height", '<?= $height ?>px')
+                            .css("margin", '0 auto')
+                            .css("z-index", '0');
+                    Reveal.initialize({
+                        width: <?= $width ?>,
+                        height: <?= $height ?>,
+                        margin: 0,
+                        embedded: true,
+                        plugins: [RevealHighlight, RevealMarkdown, RevealMath, RevealNotes, RevealSearch, RevealZoom]
+                    });
                 });
-                Reveal.initialize({
-                    controls: true,
-                    progress: false,
-                    history: true,
-                    center: true,
-                    transition: 'slide', // none/fade/slide/convex/concave/zoom
-
-                    // Optional reveal.js plugins
-                    dependencies: [
-                        {src: '../commun/src/js/reveal/lib/js/classList.js', condition: function () {
-                                return !document.body.classList;
-                            }},
-                        {src: '../commun/src/js/reveal/plugin/markdown/marked.js', condition: function () {
-                                return !!document.querySelector('[data-markdown]');
-                            }},
-                        {src: '../commun/src/js/reveal/plugin/markdown/markdown.js', condition: function () {
-                                return !!document.querySelector('[data-markdown]');
-                            }},
-                        {src: '../commun/src/js/reveal/plugin/highlight/highlight.js', async: true, callback: function () {
-                                hljs.initHighlightingOnLoad();
-                            }},
-                        {src: '../commun/src/js/reveal/plugin/zoom-js/zoom.js', async: true},
-                        {src: '../commun/src/js/reveal/plugin/notes/notes.js', async: true}
-                    ]
-                });
-            });
-        </script>
-        <?php
+            </script>
+            <?php
+            self::$_called = true;
+        }
     }
 
     /**

02767a65513ff86f6968274aa0e8591fc4eb62c6
LEGAGNEUR Matthieu <legagneur.matthieu@gmail.com>
2022-09-30 16:08:12 +0200

    Fix reveal

--- dwf/class/reveal.class.php
+++ dwf/class/reveal.class.php
@@ -17,24 +17,23 @@ class reveal {
     /**
      * Cette classe permet de créer un diaporama avec la librairie reveal, Il n'est pas recommandé d'avoir plusieurs diaporamas sur la même page !
      * Les diaporamas s'organisent avec des balises "section"
-     * @param int $width Largeur en px des slides
-     * @param int $height Hauteur en px des slides
+     * @param string $width Largeur des slides (css)
+     * @param string $height Hauteur des slides (css)
      * @param string $theme Théme des slides (white par defaut, beige,black,blood,league,noon,night,serif,simple,sky,solarized)
      */
-    public function __construct($width = 600, $height = 600, $theme = "white") {
+    public function __construct($width = "100%", $height = "300px", $theme = "white") {
         if (!self::$_called) {
-            $base = "../commun/src/js";
-            compact_css::get_instance()->add_css_file("$base/reveal/dist/reveal.css");
-            compact_css::get_instance()->add_css_file("$base/reveal/dist/theme/$theme.css");
-            compact_css::get_instance()->add_css_file("$base/reveal/plugin/highlight/monokai.css");
+            $base = "../commun/src/js/reveal/";
+            compact_css::get_instance()->add_css_file($base . "dist/reveal.css");
+            compact_css::get_instance()->add_css_file($base . "dist/theme/$theme.css");
             foreach ([
-        "$base/reveal/dist/reveal.js",
-        "$base/reveal/plugin/highlight/highlight.js",
-        "$base/reveal/plugin/markdown/markdown.js",
-        "$base/reveal/plugin/math/math.js",
-        "$base/reveal/plugin/notes/notes.js",
-        "$base/reveal/plugin/search/search.js",
-        "$base/reveal/plugin/zoom/zoom.js"
+        $base . "dist/reveal.js",
+        $base . "plugin/highlight/highlight.js",
+        $base . "plugin/markdown/markdown.js",
+        $base . "plugin/math/math.js",
+        $base . "plugin/notes/notes.js",
+        $base . "plugin/search/search.js",
+        $base . "plugin/zoom/zoom.js"
             ] as $src) {
                 echo html_structures::script($src);
             }
@@ -42,14 +41,11 @@ class reveal {
             <script type="text/javascript">
                 $(document).ready(function () {
                     $(".reveal")
-                            .css("width", '<?= $width ?>px')
-                            .css("height", '<?= $height ?>px')
+                            .css("width", '<?= $width ?>')
+                            .css("height", '<?= $height ?>')
                             .css("margin", '0 auto')
                             .css("z-index", '0');
                     Reveal.initialize({
-                        width: <?= $width ?>,
-                        height: <?= $height ?>,
-                        margin: 0,
                         embedded: true,
                         plugins: [RevealHighlight, RevealMarkdown, RevealMath, RevealNotes, RevealSearch, RevealZoom]
                     });

1a1f267ee8b1f40ecb70c7ad98474456880dac4b
LEGAGNEUR Matthieu <legagneur.matthieu@gmail.com>
2023-07-10 15:05:36 +0200

    FIX : Compatibilité entre syntaxhighlighter et datatable

--- dwf/class/syntaxhighlighter.class.php
+++ dwf/class/syntaxhighlighter.class.php
@@ -25,9 +25,9 @@ class syntaxhighlighter {
     public function __construct($code, $brush = "js", $theme = "Default") {
         echo tags::tag("pre", ["class" => "brush: " . $brush], htmlspecialchars("\r" . $code));
         if (!self::$_called) {
-            $scripts = html_structures::script("../commun/src/js/syntaxhighlighter/scripts/shCore.js");
+            $scripts = html_structures::script_defer("../commun/src/js/syntaxhighlighter/scripts/shCore.js");
             foreach (glob("../commun/src/js/syntaxhighlighter/scripts/shBrush*.js") as $fbrush) {
-                $scripts .= html_structures::script($fbrush);
+                $scripts .= html_structures::script_defer($fbrush);
             }
             echo $scripts;
             compact_css::get_instance()->add_css_file("../commun/src/js/syntaxhighlighter/styles/shCore" . $theme . ".css");
@@ -35,12 +35,13 @@ class syntaxhighlighter {
             compact_css::get_instance()->add_css_file("../commun/src/css/syntaxhighlighter.css");
             ?> 
             <script type="text/javascript">
-                (function () {
+                $(document).ready(function () {
                     SyntaxHighlighter.defaults['toolbar'] = false;
                     SyntaxHighlighter.all();
-                })();
+                });
             </script> 
             <?php
+
             self::$_called = true;
         }
     }

1dc9f365887045fce3bac2fce60f4700b156953d
Legagneur Matthieu <legagneur.matthieu@gmail.com>
2024-05-16 20:14:28 +0200

    Refonte de robotstxt.class.php

--- dwf/class/robotstxt.class.php
+++ dwf/class/robotstxt.class.php
@@ -11,13 +11,15 @@ class robotstxt {
      * Cette classe génére le robot.txt
      */
     public function __construct() {
+        $content = "User-agent: *\n";
+        if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/sitemap.xml")) {
+            $protocol = isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on" ? "https://" : "http://";
+            $domain = $_SERVER["HTTP_HOST"];
+            $content .= "Sitemap: {$protocol}{$domain}/sitemap.xml";
+        }
         if (!file_exists($robots = $_SERVER["DOCUMENT_ROOT"] . "/robots.txt")) {
-            $content = "User-agent: *\n";
-            if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/sitemap.xml")) {
-                $protocol = isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on" ? "https://" : "http://";
-                $domain = $_SERVER["HTTP_HOST"];
-                $content .= "Sitemap: {$protocol}{$domain}/sitemap.xml";
-            }
+            file_put_contents($robots, $content);
+        } elseif ((time() - filemtime($robots)) > 84600) {
             file_put_contents($robots, $content);
         }
     }

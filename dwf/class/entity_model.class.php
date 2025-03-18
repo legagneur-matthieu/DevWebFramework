6a4231765e15ce9d58c9ef2a6dfeebd2ef8e76b2
Legagneur Matthieu <legagneur.matthieu@gmail.com>
2025-03-18 16:24:18 +0100

    Fix entity_model (password)

--- dwf/class/entity_model.class.php
+++ dwf/class/entity_model.class.php
@@ -18,7 +18,7 @@ class entity_model {
                 . '<th style="border-left:0;" class="active"></th></tr><tr><th>Champ</th><th>Type</th></tr></thead><tbody>';
         foreach ($entity::get_structure() as $value) {
             $str .= '<tr><td>';
-            $pk = !in_array($value[1], ["int", "integer","bool","boolean", "string", "mail", "array"]);
+            $pk = !in_array($value[1], ["int", "integer", "bool", "boolean", "string", "mail", "array", "psw", "password"]);
             if ($value[2]) {
                 $str .= '<u>' . $value[0] . '</u>';
             } elseif ($pk) {
@@ -40,7 +40,7 @@ class entity_model {
     public static function div($entity) {
         $str = '<div class="mcd_entity"><div class="mcd_entity_name"><p>' . $entity . '</p></div><div class="mcd_entity_data"><ul>';
         foreach ($entity::get_structure() as $value) {
-            $pk = !in_array($value[1], ["int", "integer","bool","boolean", "string", "mail", "array"]);
+            $pk = !in_array($value[1], ["int", "integer", "bool", "boolean", "string", "mail", "array", "psw", "password"]);
             $str .= '<li>';
             if ($value[2]) {
                 $str .= '<u>' . $value[0] . '</u>';
@@ -49,10 +49,9 @@ class entity_model {
             } else {
                 $str .= $value[0];
             }
-            $str .= ($pk ? self::div($value[1], false) : " (" . $value[1] . ")").'</li>';
+            $str .= ($pk ? self::div($value[1], false) : " (" . $value[1] . ")") . '</li>';
         }
         $str .= '</ul></div></div>';
         return $str;
     }
-
 }

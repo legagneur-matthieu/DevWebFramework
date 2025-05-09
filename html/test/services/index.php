c8cb7cd21cfdb7949870f71cf6104cc931a7b6d2
Legagneur Matthieu <legagneur.matthieu@gmail.com>
2025-05-09 14:23:42 +0200

    revue des services

--- html/test/services/index.php
+++ html/test/services/index.php
@@ -1,19 +1,23 @@
 <?php
 
-class index_service { /** * Cette classe est la première appelée, elle ouvre les variables de session et la connexion a la base de donnée, <br /> * redéfinie la time zone et fait appel à ces méthodes privées avant d'appeler la class application (IDEM __construct()...) */
+class index_service {
 
+    /**
+     *  Cette classe est la première appelée, elle ouvre les variables de session et la connexion a la base de donnée, <br /> 
+     *  redéfinie la time zone et fait appel à ces méthodes privées avant d\'appeler la class application (IDEM __construct()...) 
+     */
     public function __construct() {
         try {
             $this->classloader();
-            include "../class/config.class.php";
             if (isset($_REQUEST["service"])) {
                 date_default_timezone_set("Europe/Paris");
+                $this->projectloader();
                 $this->entityloader();
                 application::$_bdd = new bdd();
                 session::start(false);
                 $this->serviceloader();
                 $this->security_purge();
-                $service = strtr($_REQUEST["service"], array("." => "", "/" => "", "\\" => "", "?" => "", "#" => ""));
+                $service = strtr($_REQUEST["service"], array("." => "", "/" => "", "\\\\" => "", "?" => "", "#" => ""));
                 if (file_exists($service . ".service.php")) {
                     $service = new $service();
                 } else {
@@ -27,9 +31,11 @@ class index_service { /** * Cette classe est la première appelée, elle ouvre l
         }
     }
 
-    /**     * Inclut toutes les classes du framework */
+    /**
+     * Inclut toutes les classes du framework 
+     */
     private function classloader() {
-        spl_autoload_register(function($class) {
+        spl_autoload_register(function ($class) {
             $file = __DIR__ . "/../../../dwf/class/" . $class . ".class.php";
             if (file_exists($file)) {
                 require_once $file;
@@ -37,9 +43,23 @@ class index_service { /** * Cette classe est la première appelée, elle ouvre l
         });
     }
 
-    /**     * Inclut les entités du projet */
+    /**
+     * Inclut les class du projet 
+     */
+    private function projectloader() {
+        spl_autoload_register(function ($class) {
+            $file = __DIR__ . "/../class/" . $class . ".class.php";
+            if (file_exists($file)) {
+                require_once $file;
+            }
+        });
+    }
+
+    /**
+     * Inclut les entités du projet 
+     */
     private function entityloader() {
-        spl_autoload_register(function($class) {
+        spl_autoload_register(function ($class) {
             $file = __DIR__ . "/../class/entity/" . $class . ".class.php";
             if (file_exists($file)) {
                 require_once $file;
@@ -47,9 +67,11 @@ class index_service { /** * Cette classe est la première appelée, elle ouvre l
         });
     }
 
-    /**     * Inclut toutes les classes du dossier "service" se finissant par ".service.php" */
+    /**
+     * Inclut toutes les classes du dossier "service" se finissant par ".service.php" 
+     */
     private function serviceloader() {
-        spl_autoload_register(function($class) {
+        spl_autoload_register(function ($class) {
             $file = __DIR__ . "/" . $class . ".service.php";
             if (file_exists($file)) {
                 require_once $file;
@@ -57,13 +79,16 @@ class index_service { /** * Cette classe est la première appelée, elle ouvre l
         });
     }
 
-    /**     * Supprime tous les fichiers se terminant par .php~ (trill) dans le dossier "service" <br /> * Cette fonction est recommandée sur les serveurs de production Linux pour des raisons de sécurité <br /> * certains hébergeurs tolèrent mal cette fonction, elle peut être désactivée en commentant la ligne "$this->security_purge();" dans le constructeur. */
+    /**
+     * Supprime tous les fichiers se terminant par .php~ (trill) dans le dossier "service" <br /> 
+     * Cette fonction est recommandée sur les serveurs de production Linux pour des raisons de sécurité <br /> 
+     * certains hébergeurs tolèrent mal cette fonction, elle peut être désactivée en commentant la ligne "$this->security_purge();" dans le constructeur. 
+     */
     private function security_purge() {
         foreach (glob("*.php~") as $trill) {
             unlink($trill);
         }
     }
-
 }
 
 new index_service();

230ad60cbef621cbbdd29971f3eae973596965da
Legagneur Matthieu <legagneur.matthieu@gmail.com>
2026-08-04 10:48:41 +0200

    psw hash fix

--- dwf/class/application.class.php
+++ dwf/class/application.class.php
@@ -185,6 +185,6 @@ class application {
      */
     public static function hash($data, $salt = false) {
         $sel = (isset(config::$_salt) ? config::$_salt : config::$_prefix);
-        return hash(config::$_hash_algo, ($salt ? hash(config::$_hash_algo, config::$_prefix) : "") . $data);
+        return hash(config::$_hash_algo, ($salt ? hash(config::$_hash_algo, $sel) : "") . $data);
     }
 }

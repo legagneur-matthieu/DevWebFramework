e9a3223083876927157b6166792cc45dab253849
Legagneur Matthieu <legagneur.matthieu@gmail.com>
2026-08-04 11:37:44 +0200

    fix csp nonce

--- dwf/class/jSignature.class.php
+++ dwf/class/jSignature.class.php
@@ -75,7 +75,7 @@ class jSignature {
                         tags::tag("a", ["id" => $this->_id . "_reset", "class" => "btn btn-xs btn-primary"], "Reset") .
                         tags::tag("div", ["id" => $this->_id . "_div"], "") .
                         tags::tag("input", ["type" => "hidden", "id" => $this->_id, "name" => $this->_id, "value" => ""])
-                ) . tags::tag("script", ["type" => "text/javascript"], $script);
+                ) . tags::tag("script", ["type" => "text/javascript","nonce"=> csp::get_nonce()], $script);
     }
 
 }

1227ef8825f2b18c4edefedc0f496053627069e0
Legagneur Matthieu <legagneur.matthieu@gmail.com>
2026-08-04 09:53:32 +0200

    auth : remplacement token par CSRF et suppression des migrations de vieux mot de passe

--- dwf/class/auth.class.php
+++ dwf/class/auth.class.php
@@ -29,28 +29,20 @@ class auth {
     private $_tuple_psw;
 
     /**
-     * Le formulaire doit-il posséder un token
-     * @var boolean Le formulaire doit-il posséder un token
-     */
-    private $_use_token;
-
-    /**
      * Cette classe gère l'authentification d'un utilisateur
      * 
      * @param string $table table des utilisateurs
      * @param string $tuple_login tuble du login
      * @param string $tuple_psw tuple du mot de passe
-     * @param boolean $use_token Le formulaire doit-il posséder un token (sécurité, true/false, false par defaut)
      * @param boolean $call_session_start l'application doit lancer un session_start() (true/false)
      */
-    public function __construct($table, $tuple_login, $tuple_psw, $use_token = false, $call_session_start = FALSE) {
+    public function __construct($table, $tuple_login, $tuple_psw, $call_session_start = false) {
         if ($call_session_start) {
             session_start();
         }
         $this->_table = $table;
         $this->_tuple_login = $tuple_login;
         $this->_tuple_psw = $tuple_psw;
-        $this->_use_token = $use_token;
         $this->denied_or_granted();
     }
 
@@ -67,27 +59,19 @@ class auth {
      * Affiche le formulaire d'authentification
      */
     private function form_auth() {
+        $form = new form();
         if (isset($_POST['auth_login'])) {
-            if ($this->_use_token) {
-                if ((new form())->validate_token()) {
-                    $this->exec_auth();
-                } else {
-                    js::alertify_alert_redir("Token invalide!", "");
-                }
-            } else {
+            if ($form->csrf_check()) {
                 $this->exec_auth();
             }
         } else {
             ?>
             <div class="class_auth">
                 <?php
-                $form = new form();
                 $form->input("Login", "auth_login", "text");
                 $form->input("Mot de passe", "auth_psw", "password");
+                $form->csrf_token();
                 $form->submit("btn-block btn-primary", "Connexion");
-                if ($this->_use_token) {
-                    $form->token();
-                }
                 echo $form->render();
                 ?>
             </div>
@@ -100,18 +84,13 @@ class auth {
      */
     private function exec_auth() {
         $table = $this->_table;
-        $req = $table::get_table_array("{$this->_tuple_login}=:{$this->_tuple_login} and ({$this->_tuple_psw}=:{$this->_tuple_psw} or {$this->_tuple_psw}=:{$this->_tuple_psw}_salt)", [
-                    ":{$this->_tuple_login}" => $_POST['auth_login'],
-                    ":{$this->_tuple_psw}" => application::hash($_POST['auth_psw']),
-                    ":{$this->_tuple_psw}_salt" => application::hash($_POST['auth_psw'],true)
+        $req = $table::get_table_array("{$this->_tuple_login}=:{$this->_tuple_login} and ({$this->_tuple_psw}=:{$this->_tuple_psw}", [
+            ":{$this->_tuple_login}" => $_POST['auth_login'],
+            ":{$this->_tuple_psw}" => application::hash($_POST['auth_psw'], true)
         ]);
         if (isset($req[0]["id"])) {
             session::set_auth(true);
             session::set_user($req[0]["id"]);
-            if($req[0][$this->_tuple_psw]==application::hash($_POST['auth_psw'])){
-                $set_tuple_psw="set_{$this->_tuple_psw}";
-                $table::get_from_id($req[0]["id"])->$set_tuple_psw(application::hash($_POST['auth_psw'],true)); // a test
-            }
             js::redir("");
         } else {
             $_POST["auth_psw"] = "********";
